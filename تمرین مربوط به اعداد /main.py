157459b3774a6b9b9e3284e74f16cefe8a04d41b
AMIR <golkhoshbooa@gmail.com>
2024-02-13 09:04:06 +0330

    تمرین مربوط به اعداد

---
+++ تمرین مربوط به اعداد /main.py
@@ -0,0 +1,11 @@
+# شعاع دایره را بگیرد  محیط و مساحت ان را حساب کند
+# محیط:
+from math import pi
+x = int(input("Inter a number for circle :"))
+x = x * 2 * pi
+print(x)
+#مساحت:
+from math import pi 
+x = int(input("Inter a number for circle :"))
+x = pow(x,2)*pi
+print(x)
