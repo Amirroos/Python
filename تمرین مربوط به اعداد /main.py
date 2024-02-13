a061c412e4ca81c64f7125b0000466d44e89d6ef
AMIR <golkhoshbooa@gmail.com>
2024-02-13 09:23:28 +0330

    new change

--- تمرین مربوط به اعداد /main.py
+++ تمرین مربوط به اعداد /main.py
@@ -9,3 +9,20 @@ from math import pi
 x = int(input("Inter a number for circle :"))
 x = pow(x,2)*pi
 print(x)
+# برنامه ای بنویسید ک از کاربر یه عدد را گرفته و مربع و مکعب ان را حساب کند
+x = int(input("Inter number :"))
+b = pow(x,2)
+print("The square is :",b)
+x = pow(x,3)
+print("The cube is :",x)
+# برنامه ای بنویسید ک دو عدد را از کاربر گرفته و اولی را به توان دومی برساند 
+x = int(input("Inter first number :"))
+b = int(input("Inter second number :"))
+z = pow(x,b)
+print(z)
+# برنامه بنویسید ک سه عدد را از کاربر گرفته و میانگین ان هارا حساب کند
+x = int(input("Inter first number :"))
+x += int(input("Inter second number :"))
+x += int(input("Inter third number :"))
+x /= 3
+print(x)
